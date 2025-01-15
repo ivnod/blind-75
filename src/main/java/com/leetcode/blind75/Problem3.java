79a87f78cbc9e4a0d16161189770f61304e395cf
VInod <vinodreddymedagam@gmail.com>
2025-01-15 13:40:11 +0800

    Product except Self

--- src/main/java/com/leetcode/blind75/Problem3.java
+++ src/main/java/com/leetcode/blind75/Problem3.java
@@ -16,10 +16,10 @@ public class Problem3 {
 
   static class Solution {
     public boolean containsDuplicate(int[] nums) {
-      HashSet<Integer> hashSet = new HashSet<>();
+      HashSet<Integer> visitedNumbers = new HashSet<>();
       for (int i = 0; i < nums.length; i++) {
-        if (hashSet.contains(nums[i])) return true;
-        else hashSet.add(nums[i]);
+        if (visitedNumbers.contains(nums[i])) return true;
+        else visitedNumbers.add(nums[i]);
       }
       return false;
     }
